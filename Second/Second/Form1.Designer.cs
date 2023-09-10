5a0337e387ac65c36925d8a9096e7284da337bda
Bogdan Skripchenko <SkBogdan@bk.ru>
2023-09-10 21:46:05 +0500

    Оранжевый фон и меньше форма

--- Second/Second/Form1.Designer.cs
+++ Second/Second/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 295);
+            this.button1.Location = new System.Drawing.Point(182, 177);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 46);
             this.button1.TabIndex = 0;
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(498, 288);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Second project";
