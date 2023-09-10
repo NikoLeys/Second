5f428a5bb2f4f65c2c7672979940b0cfa24ed6c3
Bogdan Skripchenko <SkBogdan@bk.ru>
2023-09-10 21:48:21 +0500

    Светло зеленый фон

--- Second/Second/Form1.Designer.cs
+++ Second/Second/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Green;
+            this.BackColor = System.Drawing.Color.LightGreen;
             this.ClientSize = new System.Drawing.Size(498, 288);
             this.Name = "Form1";
             this.Text = "Second project";
